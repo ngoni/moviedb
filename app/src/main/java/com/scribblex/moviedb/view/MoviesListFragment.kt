1e433661e4f16aa2f9c9f639f63ea5fd7333946c
Ngoni Munyaradzi <ngoni.munyaradzi@rapiddeploy.com>
2020-07-24 18:49:56 +0200

    initial commit

---
+++ app/src/main/java/com/scribblex/moviedb/view/MoviesListFragment.kt
@@ -0,0 +1,40 @@
+package com.scribblex.moviedb.view
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.NavController
+import androidx.navigation.NavDirections
+import androidx.navigation.Navigation
+import com.scribblex.moviedb.R
+import com.scribblex.moviedb.databinding.FragmentMoviesListBinding
+
+class MoviesListFragment : Fragment() {
+
+    private lateinit var binding: FragmentMoviesListBinding
+    private lateinit var navController: NavController
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentMoviesListBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initButton()
+        navController = Navigation.findNavController(view)
+    }
+
+    private fun initButton() {
+        binding.button.text = "Click Here"
+        binding.button.setOnClickListener {
+            navController.navigate(R.id.movieList_to_movieDetailScreen)
+        }
+    }
+}
