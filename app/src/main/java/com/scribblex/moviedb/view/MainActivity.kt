fd954bfd0d46f6a554d9de8491ca86ea23443bd2
Ngoni Munyaradzi <ngoni.munyaradzi@gmail.com>
2020-07-25 22:37:03 +0200

    Hilt 2.28-aplha

--- app/src/main/java/com/scribblex/moviedb/view/MainActivity.kt
+++ app/src/main/java/com/scribblex/moviedb/view/MainActivity.kt
@@ -5,7 +5,9 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import com.scribblex.moviedb.R
 import com.scribblex.moviedb.databinding.ActivityMainBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
--- app/src/main/java/com/scribblex/moviedb/view/MovieDetailFragment.kt
+++ app/src/main/java/com/scribblex/moviedb/view/MovieDetailFragment.kt
@@ -6,7 +6,9 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.scribblex.moviedb.databinding.FragmentMovieDetailBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MovieDetailFragment : Fragment() {
 
     private lateinit var binding: FragmentMovieDetailBinding
